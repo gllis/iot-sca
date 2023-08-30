edc74a00b315907f5d2b32f562ea77069190aea9
gllis <794669024@qq.com>
2023-08-30 23:16:14 +0800

    first commit

---
+++ iot-sca-gateway/src/main/java/com/gllis/iot/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.gllis.iot.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Cloud 网关
+ *
+ * @author glli
+ * @date 2023/8/29
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
